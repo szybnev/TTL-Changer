e82630af294558c7803bd8d8fbfb00982247081f
Zybnev Sergey <32132485+szybnev@users.noreply.github.com>
2021-12-19 22:28:46 +0300

    Add files via upload

---
+++ 128.bat
@@ -0,0 +1,2 @@
+netsh int ipv4 set glob defaultcurhoplimit=128
+netsh int ipv6 set glob defaultcurhoplimit=128
---
+++ 65.bat
@@ -0,0 +1,2 @@
+netsh int ipv4 set glob defaultcurhoplimit=65
+netsh int ipv6 set glob defaultcurhoplimit=65
